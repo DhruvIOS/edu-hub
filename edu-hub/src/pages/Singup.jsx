2236192deb9eee08abe571582a6163c7decedc40
Arya Bhatt <aryabhatt2023@gmail.com>
2025-04-12 09:24:46 -0400

    New design

--- edu-hub/src/pages/Singup.jsx
+++ edu-hub/src/pages/Singup.jsx
@@ -2,60 +2,58 @@ import { useState } from "react"
 import { auth } from "../firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth"
 
+import { useAuth } from "../context/AuthContext";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Signup = () => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  // const initialRole = searchParams.get("role") || "student";
 
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [role, setRole] = useState("Student");
+  const [isVisible, setVisible] = useState(false);
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [role, setRole] = useState("Student");
-    const [isVisible, setVisible] = useState(false);
+  const handleSingup = async(e) => {
+    e.preventDefault();
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+        const idToken = await userCredential.user.getIdToken();
 
-    const handleDropDownChange = (value) =>{
+        //Send user data to backend
 
-        setVisible(value === "TA")
-        
-    }
-
-    const handleSingup = async(e) => {
-        e.preventDefault();
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-            const idToken = await userCredential.user.getIdToken();
-
-            //Send user data to backend
-
-            await fetch ("http://localhost:5000/api/auth/signup", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                   firstName, lastName, email, role, password,token: idToken}),
-            }) .then((response) => response.json())
-            .then((data) => {
-              
-
-                alert(data.message)
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
+        await fetch ("http://localhost:5000/api/auth/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+               firstName, lastName, email, role, password,token: idToken}),
+        }) .then((response) => response.json())
+        .then((data) => {
           
 
-           
-        } catch (error) {
-            console.error("Error creating user:", error)
-            alert(error)
-        }
-    };
+            alert(data.message)
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 
-    return (
+      
 
-        <div className="dashboard login-box">
+       
+    } catch (error) {
+        console.error("Error creating user:", error)
+        alert(error)
+    }
+};
+  return (
+    <div className="dashboard login-box">
       <h2 className="text-center">Sign Up to EDU-HUB</h2>
 
       <form onSubmit={handleSingup}>
@@ -72,15 +70,6 @@ const Signup = () => {
 
         <input
           className="input-field"
-          type="email"
-          placeholder="Your Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-
-<input
-          className="input-field"
           type="name"
           placeholder="Your name"
           value={firstName}
@@ -91,11 +80,21 @@ const Signup = () => {
 <input
           className="input-field"
           type="name"
-          placeholder="Your name"
+          placeholder="Your last name"
           value={lastName}
           onChange={(e) => setLastName(e.target.value)}
           required
         />
+
+        <input
+          className="input-field"
+          type="email"
+          placeholder="Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
         <input
           className="input-field"
           type="password"
@@ -104,8 +103,8 @@ const Signup = () => {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-
-        {/* {role === "ta" && (
+{/* 
+        {role === "TA" && (
           <input
             className="input-field ta-box"
             type="email"
@@ -121,12 +120,7 @@ const Signup = () => {
         </button>
       </form>
     </div>
+  );
+};
 
-
-    
-    )
-
-
-}
-
-export default Signup
+export default Signup;
