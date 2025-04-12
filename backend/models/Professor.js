a0ddc7f86660aff422383fcc552da1a1b8926dde
DhruvIOS <shahdhruv684@gmail.com>
2025-04-12 18:01:21 -0400

    fixed issue

--- backend/models/Professor.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const ProfessorSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
-});
-
-export default mongoose.model('Professor', ProfessorSchema);
